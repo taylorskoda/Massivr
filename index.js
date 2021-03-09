f57170aa8850cc199a4ecfe5e19634c25fae4495
taylorskoda <79763930+taylorskoda@users.noreply.github.com>
2021-03-09 03:44:36 -0500

    Update index.js

--- index.js
+++ index.js
@@ -13,4 +13,4 @@ client.aliases = new discord.Collection();
 util.loadModules("events");
 util.loadModules("commands", true)
 
-client.login("YOUR TOKEN");
+client.login("ODE1NzQ3MzM5OTg1NjE2OTA3.YEc0wQ.3MfLZLQHnM66i2AZmyDQ2U1bCR0");
